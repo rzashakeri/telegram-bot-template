903e92358acec9ef24c7e40ade5f7fd957d056bd
Reza shakeri <rzashakeri@gmail.com>
2023-05-21 19:44:47 +0330

    ✨ feat: added main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from telegram.ext import Application
+from telegram.ext import CommandHandler
+from telegram.ext import filters
+from telegram.ext import MessageHandler
+
+from commands.maintenance import maintenance
+from configurations import settings
+from configurations.settings import IS_MAINTENANCE
+from utils import create_requirement_folders
+from utils import logger
+
+if __name__ == "__main__":
+    logger.init_logger(f"logs/{settings.NAME}.log")
+    create_requirement_folders()
+    application = (Application.builder().token(
+        settings.TOKEN).read_timeout(50).write_timeout(
+            50).get_updates_read_timeout(50).persistence(persistence).build())
+    if IS_MAINTENANCE:
+        application.add_handler(CommandHandler("start", maintenance))
+        application.add_handler(
+            MessageHandler(filters.TEXT & ~filters.COMMAND, maintenance))
+    else:
+        """YOUR COMMANDS IS HERE WHEN BOT IS NOT MAINTENANCE"""
+    application.run_polling()
