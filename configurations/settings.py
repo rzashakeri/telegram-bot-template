946eca110aab5274cca1febdaaf9678790efb368
Reza shakeri <rzashakeri@gmail.com>
2023-05-21 19:49:51 +0330

    ✨ feat: added settings.py

---
+++ configurations/settings.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.environ["TOKEN"]
+NAME = os.environ["NAME"]
+ADMIN_TELEGRAM_USER_ID = int(os.environ["ADMIN_TELEGRAM_USER_ID"])
+IS_MAINTENANCE = bool(os.environ["IS_MAINTENANCE"])
+LIST_OF_ADMINS = [ADMIN_TELEGRAM_USER_ID]
+WEBHOOK = False
+# The following configuration is only needed if you setted WEBHOOK to True #
+WEBHOOK_OPTIONS = {
+    "listen": "0.0.0.0",  # IP
+    "port": 443,
+    "url_path": TOKEN,  # This is recommended for avoiding random people
+    # making fake updates to your bot
+    "webhook_url": f"https://example.com/{TOKEN}",
+}
