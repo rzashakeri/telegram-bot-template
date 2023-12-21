c728fea6ed67ab1426eb09ac3c94de108723a21b
Reza shakeri <rzashakeri@gmail.com>
2023-12-22 03:16:29 +0330

    ♻️ refactor: Add PicklePersistence

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from telegram.ext import Application
 from telegram.ext import CommandHandler
 from telegram.ext import filters
 from telegram.ext import MessageHandler
+from telegram.ext import PicklePersistence
 
 from commands.maintenance import maintenance
 from configurations import settings
@@ -10,6 +11,7 @@ from utils import logger
 
 if __name__ == "__main__":
     logger.init_logger(f"logs/{settings.NAME}.log")
+    persistence = PicklePersistence(filepath="conversation states")
     application = (Application.builder().token(
         settings.TOKEN).read_timeout(50).write_timeout(
             50).get_updates_read_timeout(50).persistence(persistence).build())
